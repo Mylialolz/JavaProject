0cbb35cd1ad15124a96e90790c3da92ff47560b7
WerlingJ <werlingjoris@hotmail.fr>
2016-11-26 17:29:13 +0100

    Création de la FSM Architecture Finie, TODO : Mettre en place les fonctions à appeler dans le SWITCH et peut-être mettre en place un Timer qui réappelera la fonction Machine à la fin du temps attribué à chaque tâches

---
+++ FSM.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Joris on 25/11/2016.
+ */
+public class FSM {
+
+   private int nbRoundPlayed;
+
+   private int currentState; // Indicates the current state of the FSM
+
+   public void FSM(){
+       currentState = 0;
+       nbRoundPlayed = 0;
+   }
+
+   private int Machine(){
+       int timerOrder = currentState;
+       switch(currentState){
+           case 0 : //STATE ROUND
+               nbRoundPlayed ++;
+               if(nbRoundPlayed == 6){
+                   currentState = 2;
+                   /*CALL THE FUNCTION WHICH HANDLES THE ROUND*/
+               }
+               else{
+                   currentState = 1;
+                   /*CALL THE FUNCTION WHICH HANDLES THE ROUND*/
+               }
+               break;
+           case 1 : //STATE DISPLAY ROUND SCORES
+               currentState = 0;
+               /*CALL THE FUNCTION WHICH DISPLAY THE ROUND SCORES*/
+               break;
+           case 2 : // STATE DISPLAY GAME SCORES
+               break;
+           }
+       return currentState; // A retirer si on met le timer dans la FSM
+   }
+
+
+}
+
+
+
